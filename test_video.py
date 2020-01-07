1636f431bc29126a2d6d5986142c8c53906c0d1b
blu <lzbgt@126.com>
2020-01-07 13:59:21 +0800

    init

--- test_video.py
+++ test_video.py
@@ -13,14 +13,14 @@ def get_args():
     parser.add_argument("--nms_threshold", type=float, default=0.5)
     parser.add_argument("--pretrained_model", type=str, default="trained_models/signatrix_efficientdet_coco.pth")
     parser.add_argument("--input", type=str, default="test_videos/input.mp4")
-    parser.add_argument("--output", type=str, default="test_videos/output.mp4")
+    parser.add_argument("--output", type=str, default="output.mp4")
 
     args = parser.parse_args()
     return args
 
 
 def test(opt):
-    model = torch.load(opt.pretrained_model).module
+    model = torch.load(opt.pretrained_model, map_location='cpu').module
     if torch.cuda.is_available():
         model.cuda()
 
